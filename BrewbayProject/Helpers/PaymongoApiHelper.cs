abe418eb1760784ad2c48f57e846a012008e9385
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2023-12-10 19:07:39 +0800

    encode paymongo secret key to base64

--- BrewbayProject/Helpers/PaymongoApiHelper.cs
+++ BrewbayProject/Helpers/PaymongoApiHelper.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace BrewbayProject.Helpers;
 
 public class PaymongoApiHelper
@@ -6,11 +8,17 @@ public class PaymongoApiHelper
     {
         var client = new HttpClient();
         var secretKey = config.GetValue<String>("PaymongoSecretKey");
+        var encodedKey = EncodeToBase64(secretKey!);
 
         client.BaseAddress = new Uri("https://api.paymongo.com/v1/");
         client.DefaultRequestHeaders.Add("Accept", "applicaiton/json");
-        client.DefaultRequestHeaders.Add("Authorization", $"Basic c2tfdGVzdF9Ma2IxWGdxZVdKVXFDbzNCeVdONzM1a2k=");
+        client.DefaultRequestHeaders.Add("Authorization", $"Basic {encodedKey}");
 
         return client;
+    }
+
+    private static string EncodeToBase64(string key)
+    {
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(key));
     }
 }
